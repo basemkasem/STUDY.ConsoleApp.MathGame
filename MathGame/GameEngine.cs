fa9d34cf839c85c7cda9a613f8bfd122152fc9bb
basem <basem.eng_5261@eng.kfs.edu.eg>
2024-03-22 04:28:01 +0200

    Add menu class Add GameEngine class Add Helpers class

---
+++ MathGame/GameEngine.cs
@@ -0,0 +1,128 @@
+﻿namespace MathGame
+{
+    internal class GameEngine
+    {
+        internal void AdditionGame(string message)
+        {
+            Console.Clear();
+            Console.WriteLine(message);
+            var random = new Random();
+            var score = 0;
+
+            int firstNumber;
+            int secondNumber;
+            for (int i = 0; i < 5; i++)
+            {
+                firstNumber = random.Next(1, 9);
+                secondNumber = random.Next(1, 9);
+                Console.Write($"{firstNumber} + {secondNumber} = ");
+                var result = Console.ReadLine();
+                if (int.Parse(result) == firstNumber + secondNumber)
+                {
+                    Console.WriteLine("Correct answer. Type any key for the next question");
+                    score++;
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Wrong answer. Type any key for the next question");
+                    Console.ReadLine();
+                }
+            }
+            Console.WriteLine($"Game Over. Your score is {score}.Press Any key to go back to menu.");
+            Console.ReadLine();
+            Helpers.AddToHistory(score, "Addition");
+        }
+        internal void SubtractionGame(string message)
+        {
+            Console.Clear();
+            Console.WriteLine(message);
+            var random = new Random();
+            var score = 0;
+
+            int firstNumber;
+            int secondNumber;
+            for (int i = 0; i < 5; i++)
+            {
+                firstNumber = random.Next(1, 9);
+                secondNumber = random.Next(1, 9);
+                Console.Write($"{firstNumber} - {secondNumber} = ");
+                var result = Console.ReadLine();
+                if (int.Parse(result) == firstNumber - secondNumber)
+                {
+                    Console.WriteLine("Correct answer. Type any key for the next question");
+                    score++;
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Wrong answer. Type any key for the next question");
+                    Console.ReadLine();
+                }
+            }
+            Console.WriteLine($"Game Over. Your score is {score}.Press Any kkey to go back to menu.");
+            Console.ReadLine();
+            Helpers.AddToHistory(score, "subtraction");
+        }
+        internal void MultiplicationGame(string message)
+        {
+            Console.Clear();
+            Console.WriteLine(message);
+            var random = new Random();
+            var score = 0;
+
+            int firstNumber;
+            int secondNumber;
+            for (int i = 0; i < 5; i++)
+            {
+                firstNumber = random.Next(1, 9);
+                secondNumber = random.Next(1, 9);
+                Console.Write($"{firstNumber} * {secondNumber} = ");
+                var result = Console.ReadLine();
+                if (int.Parse(result) == firstNumber * secondNumber)
+                {
+                    Console.WriteLine("Correct answer. Type any key for the next question");
+                    score++;
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Wrong answer. Type any key for the next question");
+                    Console.ReadLine();
+                }
+            }
+            Console.WriteLine($"Game Over. Your score is {score}.Press Any kkey to go back to menu.");
+            Console.ReadLine();
+            Helpers.AddToHistory(score, "Multiplication");
+        }
+        internal void DivisionGame(string message)
+        {
+            Console.Clear();
+            Console.WriteLine(message);
+            var score = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                var divisionNumbers = Helpers.GetDivisionNumbers();
+                var firstNumber = divisionNumbers[0];
+                var secondNumber = divisionNumbers[1];
+
+                Console.Write($"{firstNumber} / {secondNumber} = ");
+                var result = Console.ReadLine();
+                if (int.Parse(result) == firstNumber / secondNumber)
+                {
+                    Console.WriteLine("Correct answer. Type any key for the next question");
+                    score++;
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Wrong answer. Type any key for the next question");
+                    Console.ReadLine();
+                }
+            }
+            Console.WriteLine($"Game Over. Your score is {score}.Press Any kkey to go back to menu.");
+            Console.ReadLine();
+            Helpers.AddToHistory(score, "Division");
+        }
+    }
+}
---
+++ MathGame/Helpers.cs
@@ -0,0 +1,48 @@
+﻿namespace MathGame
+{
+    internal class Helpers
+    {
+        static List<string> games = new();
+        internal static string GetName()
+        {
+            Console.Write("Enter your name: ");
+            var name = Console.ReadLine();
+            return name;
+        }
+        internal static void GetGames()
+        {
+            Console.Clear();
+            Console.WriteLine("Games History: ");
+            foreach (var game in games)
+            {
+                Console.WriteLine(game);
+            }
+            Console.WriteLine("Press any key to go to the menu.");
+            Console.ReadLine();
+        }
+        internal static void AddToHistory(int gameScore, string gameType)
+        {
+            games.Add($"{DateTime.Now} - {gameType}: {gameScore} pts.");
+        }
+        internal static int[] GetDivisionNumbers()
+        {
+            var random = new Random();
+
+            int firstNumber;
+            int secondNumber;
+
+            var result = new int[2];
+
+            do
+            {
+                firstNumber = random.Next(1, 99);
+                secondNumber = random.Next(1, 99);
+            } while (firstNumber % secondNumber != 0);
+
+            result[0] = firstNumber;
+            result[1] = secondNumber;
+
+            return result;
+        }
+    }
+}
